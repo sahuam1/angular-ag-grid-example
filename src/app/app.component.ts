25f6c487d77eb5b7415bb14be47aa3d8fcb33223
Sumit <sumitsahuec@gmail.com>
2021-10-11 10:11:01 +0530

    mt test stash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angr ag-id';
+  title = 'ular ag-grid';
   sg='My Name';
 
   columnDefs = [
