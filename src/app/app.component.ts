7a5ca4e9c4f29b6f62b024bbfe8743b3d39e10d8
sahuam1 <amitsahube@gmail.com>
2021-10-11 10:01:36 +0530

    Update app.component.ts
    dasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular ag-grid';
+  title = 'Angr ag-id';
   sg='My Name';
 
   columnDefs = [
